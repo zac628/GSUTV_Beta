1dfb8545814f403189ae496e4f9f3b88e932317d
zac628 <zac628@gmail.com>
2018-04-13 11:37:10 -0400

    Initial Commit

---
+++ GSUTV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GSUTV
+//
+//  Created by Zachary Cox on 4/13/18.
+//  Copyright © 2018 Zachary Cox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
