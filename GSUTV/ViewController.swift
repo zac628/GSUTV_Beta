9f38a72aa72f98aa7241053ce9fb596940d794f1
zac628 <zac628@gmail.com>
2018-08-30 00:44:02 -0400

    useless shit

--- GSUTV/ViewController.swift
+++ GSUTV/ViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import SQLite3
 
 class ViewController: UIViewController {
-
+    
+     var db: OpaquePointer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //the database file
+        print("does it work")
+        
+        let fileURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            .appendingPathComponent("GSUTVdb.sqlite3")
+        
+        //opening the database  
+        if sqlite3_open(fileURL.path, &db) != SQLITE_OK {
+            print("error opening database")
+        }
+        
+        let createTable = "CREATE TABLE IF NOT EXISTS Heroes (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, powerrank INTEGER)"
+        //creating table
+        if sqlite3_exec(db, createTable, nil, nil, nil) != SQLITE_OK {
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("error creating table: \(errmsg)")
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
