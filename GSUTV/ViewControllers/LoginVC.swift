19661cc8d4eab220e081b3f5be14f9b3d2a8da08
zac628 <zac628@gmail.com>
2018-08-30 15:57:09 -0400

    Create Productions & Users

--- GSUTV/ViewControllers/LoginVC.swift
+++ GSUTV/ViewControllers/LoginVC.swift
@@ -21,6 +21,27 @@ class LoginVC: UIViewController {
             performSegue(withIdentifier: "segueLoginLanding", sender: nil)
         }
     }
+    
+    
+    @IBOutlet weak var UInewuser: UITextField!
+    @IBOutlet weak var UInewpass: UITextField!
+    @IBOutlet weak var UInewpass2: UITextField!
+    @IBAction func NewRegisterBtn(_ sender: Any) {
+        let newu: String = UInewuser.text!
+        let newp: String = UInewpass.text!
+        if(Login().check(uname: newu, upass: newp)){
+            print("Already a registered user")
+        }else{
+            if(Login().addUser(uname: newu, upass: newp) == -1){
+                print("fail")
+            }else{
+                dismiss(animated: true, completion: nil)
+            }
+        }
+        //navigationController?.popViewController(animated: true)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
